9d31051ca62a2717e9d39875bff0ecd8e5b2b4bc
Youssef Mohamed Abd alhamid <11410120240709@stud.cu.edu.eg>
2026-09-10 02:43:45 +0300

    Add files via upload

---
+++ Bike.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using task8.Interface;
+
+namespace task8
+{
+    internal class Bike : IVehicle
+    {
+        public void StartEngine()
+        {
+            Console.WriteLine("Bike engine started");
+        }
+
+        public void StopEngine()
+        {
+            Console.WriteLine("Bike engine stopped");
+        }
+    }
+}
